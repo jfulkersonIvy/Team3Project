99983d97981338e3220a7e7df062082de9912af3
craftont <craftont@iu.edu>
2023-11-13 19:45:09 -0500

    Create book.java

---
+++ book.java
@@ -0,0 +1,52 @@
+package library;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private int ISBN;
+    private boolean available;
+
+    // Constructor
+
+    public Book(String title, String author, int ISBN) {
+
+    }
+
+    // Get book title
+
+    public String getTitle() {
+
+    }
+
+    // Get book author
+
+    public String getAuthor() {
+
+    }
+
+    // Get book ISBN
+
+    public int getISBN() {
+
+    }
+
+    // Identifiy book availability
+
+    public boolean isAvailable() {
+
+    }
+
+    // If available, check out book and make unavailable
+
+    public void chekcoutBook() {
+
+    }
+
+    // If checked out, return book
+
+    public void returnBook() {
+
+    }
+
+}
